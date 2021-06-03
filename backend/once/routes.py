6c64394176942828b0a20b8a7dd6ff037fcec2ec
Galen Guyer <galen@galenguyer.com>
2021-06-02 21:31:56 -0700

    change create route and return id

--- backend/once/routes.py
+++ backend/once/routes.py
@@ -2,11 +2,11 @@ from once import once
 from flask import request, jsonify
 
 
-@once.route("/api/v1/secrets", methods=["POST"])
-def _post_api_v1_secrets():
+@once.route("/api/v1/secrets/<id>", methods=["POST"])
+def _post_api_v1_secrets(id):
     if request.json is None:
         return jsonify({"error": "data is not recognized as valid json"}), 400
-    return "", 204
+    return jsonify({"id": id})
 
 
 @once.route("/api/v1/secrets", methods=["GET"])
