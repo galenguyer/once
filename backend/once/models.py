6f6a70203c9dab2a95d77ae18a655fcfcfe713ec
Galen Guyer <galen@galenguyer.com>
2021-06-02 21:42:29 -0700

    Fix db init

--- backend/once/models.py
+++ backend/once/models.py
@@ -8,10 +8,20 @@ class Secret(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     created_by = db.Column(db.String(128))
     created_email = db.Column(db.String(128))
-    max_views = db.Column(db.Integer, default=1)
+    max_views = db.Column(db.Integer, nullable=False, default=1)
     delete_after = db.Column(
         db.DateTime, default=(datetime.utcnow() + timedelta(days=7))
     )
 
     def __repr__(self):
         return f"<Secret {id}>"
+
+    def __dict__(self):
+        d = {}
+        d["id"] = self.id
+        d["data"] = self.data
+        d["created_at"] = str(self.created_at)
+        d["created_by"] = str(self.created_by) or "Anonymous"
+        d["created_email"] = str(self.created_email) or "none"
+        d["max_views"] = self.max_views
+        d["delete_after"] = str(self.delete_after) or ""
