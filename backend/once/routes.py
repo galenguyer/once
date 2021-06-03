5dffc345f44ca09d4b63c4eebdf72aeab7d6fa11
Galen Guyer <galen@galenguyer.com>
2021-06-02 17:49:27 -0700

    Add check for valid json

--- backend/once/routes.py
+++ backend/once/routes.py
@@ -1,5 +1,9 @@
 from once import once
+from flask import request, jsonify
+
 
 @once.route("/api/v1/create", methods=["POST"])
 def _post_api_v1_create():
+    if request.json is None:
+        return jsonify({"error": "data is not recognized as valid json"}), 400
     return "", 204
