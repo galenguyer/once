222b3d4c146d590292fc07dab759a0beff7034ca
Galen Guyer <galen@galenguyer.com>
2021-06-02 21:30:59 -0700

    Add 404 error route

--- backend/once/errors.py
+++ backend/once/errors.py
@@ -5,3 +5,7 @@ from flask import request, jsonify
 @once.errorhandler(400)
 def _error_400(error):
     return jsonify({"error": "bad request", "details": str(error)}), 400
+
+@once.errorhandler(404)
+def _error_404(error):
+    return jsonify({"error": "not found"}), 404
